749dcbd7c9671e3e9d5de6400603fda0350e0781
huyenhuyen <nguyenhuyen98pm@gmail.com>
2019-07-03 22:56:59 +0700

    Student management sample code

---
+++ StudentManagement/StudentManagement.java
@@ -0,0 +1,25 @@
+public class StudentManagement {
+
+    // TODO: khai bao thuoc tinh students la array chua cac đoi tuong thuoc lop Student (max. 100)
+
+    public static boolean sameGroup(Student s1, Student s2) {
+        // TODO:
+        return false; // xoa dong nay sau khi cai đat
+    }
+
+    public void addStudent(Student newStudent){
+	 // TODO:
+    }
+
+    public String studentsByGroup() {
+        // TODO:
+    }
+
+    public void removeStudent(String id) {
+        // TODO:
+    }
+
+    public static void main(String[] args) {
+        // TODO:
+    }
+}
