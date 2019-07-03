996acd932bbc6703cb52c170e15d47ca27d2b2c3
huyenhuyen <nguyenhuyen98pm@gmail.com>
2019-07-03 22:35:52 +0700

    Student management sample code

---
+++ Student.java
@@ -0,0 +1,36 @@
+public class Student {
+
+    // TODO: khai bao cac thuoc tinh cho Student
+
+    // TODO: khai bao cac phuong thuc getter, setter cho Student
+
+    /**
+     * Constructor 1
+     */
+    Student() {
+        // TODO:
+    }
+
+    /**
+     * Constructor 2
+     * @param n
+     * @param sid
+     * @param em
+     */
+    Student(String name, String id, String email) {
+        // TODO:
+    }
+
+    /**
+     * Constructor 3
+     * @param s
+     */
+    Student(Student s) {
+        // TODO:
+    }
+
+    String getInfo() {
+        // TODO:
+        return null; // xoa dong nay sau khi cai dat
+    }
+}
