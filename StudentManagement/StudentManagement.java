845300a26a6ca5da3149931dd3b476a2957878b7
Bui Quang Cuong <bqcuong2212@gmail.com>
2019-07-05 13:33:45 +0700

    Update StudentManagement.java

--- StudentManagement/StudentManagement.java
+++ StudentManagement/StudentManagement.java
@@ -1,10 +1,10 @@
 public class StudentManagement {
 
-    // TODO: khai bao thuoc tinh students la array chua cac đoi tuong thuoc lop Student (max. 100)
+    // TODO: khai bao thuoc tinh students la array chua cac doi tuong thuoc lop Student (max. 100)
 
     public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xoa dong nay sau khi cai đat
+        return false; // xoa dong nay sau khi cai dat
     }
 
     public void addStudent(Student newStudent){
